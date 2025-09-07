5c0b679a9c7d123ca6b25c1a6218731123aa46bf
enryu <67031019+0xabysss@users.noreply.github.com>
2025-09-07 09:11:10 +0100

    update: added the friend service to the ServiceManager's interface ...

--- src/types/service-manager.d.ts
+++ src/types/service-manager.d.ts
@@ -1,7 +1,9 @@
 import type UserService from "../services/user.ts";
+import type FriendService from "../services/friend.js";
 
 
 
 export interface ServiceManager {
     user: UserService;
+    friend: FriendService;
 }
