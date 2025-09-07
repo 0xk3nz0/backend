b9761cccf609ef44eb95fc19779551e8560f60dd
enryu <67031019+0xabysss@users.noreply.github.com>
2025-09-07 09:10:51 +0100

    update: added the friend service to the ServiceManager!

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,12 +1,14 @@
 import type { FastifyInstance } from "fastify";
 import type { ServiceManager } from "../types/service-manager.js";
 import UserService from "./user.js";
+import FriendService from "./friend.js";
 
 
 
 export function buildServiceManager(fastify: FastifyInstance): ServiceManager {
     return {
         user: new UserService(fastify),
-        /// add more services when needed!
+        friend: new FriendService(fastify)
+        /// add other services ...
     };
 }
