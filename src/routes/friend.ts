29b5ce7e3166aa420599debdc855351ab18e4c81
enryu <67031019+0xabysss@users.noreply.github.com>
2025-09-07 09:10:23 +0100

    update: added routes to the routes/friend boiler plate

--- src/routes/friend.ts
+++ src/routes/friend.ts
@@ -1,7 +1,29 @@
+import { getFriendsController, getPendingRequestsController, resolveFriendRequestController, sendFriendRequestController } from "../controllers/friend.js";
+import { getFriendsSchema, getPendingRequestsSchema, resolveFriendRequestSchema, sendFriendRequestSchema } from "../schemas/friend.js";
 import type {FastifyInstance, FastifyPluginOptions} from "fastify";
 
 
 
 export default async (fastify: FastifyInstance, options: FastifyPluginOptions): Promise<void> => {
+
+    fastify.post('/request', {
+        schema: sendFriendRequestSchema,
+        handler: sendFriendRequestController
+    });
+
+    fastify.put('/respond', {
+        schema: resolveFriendRequestSchema,
+        handler: resolveFriendRequestController
+    });
+
+    fastify.get('/friends', {
+        schema: getFriendsSchema,
+        handler: getFriendsController
+    });
+
+    fastify.get('/pending', {
+        schema: getPendingRequestsSchema,
+        handler: getPendingRequestsController
+    });
 
 };
