2f8e8135ce3210e2f0cda4e017a2d40d4dd1ade5
enryu <67031019+0xabysss@users.noreply.github.com>
2025-09-07 09:10:02 +0100

    update: added models for the controller inputs

--- src/models/friend.ts
+++ src/models/friend.ts
@@ -5,6 +5,15 @@ import type {FriendRequestStatus} from "../types/friend.js";
 
 export type FriendRequestStatus_t = (typeof FriendRequestStatus)[keyof typeof FriendRequestStatus]
 
+export interface SendFriendRequestInput {
+    requested_uid: string;
+};
+
+export interface ResolveFriendRequestInput {
+    requested_uid: string; // the user that the friend request was sent from
+    action: boolean;
+};
+
 export default interface FriendRequest {
     id?: string;
     requester: UserModel;
