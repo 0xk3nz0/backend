906467b6277ebb2ea669313ed4f8c9baad3b6a20
enryu <67031019+0xabysss@users.noreply.github.com>
2025-09-09 19:57:27 +0100

    update: nothing much!

--- src/plugins/jwt.ts
+++ src/plugins/jwt.ts
@@ -2,9 +2,10 @@ import type { FastifyInstance } from "fastify";
 import fp from "fastify-plugin";
 
 import { JWTAuthentication } from "../middleware/user.js";
+import type { FastifyPluginOptions } from "fastify/types/plugin.js";
 
 
 
-export default fp(async (fastify: FastifyInstance) => {
+export default fp(async (fastify: FastifyInstance, opts: FastifyPluginOptions) => {
     fastify.decorate('authentication_jwt', JWTAuthentication);
 });
