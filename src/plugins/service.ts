3d951e928ff75774b70f00ee27af40f887a7ca62
enryu <67031019+0xabysss@users.noreply.github.com>
2025-09-09 19:57:32 +0100

    update: nothing much!

--- src/plugins/service.ts
+++ src/plugins/service.ts
@@ -1,9 +1,10 @@
 import type { FastifyInstance } from "fastify";
 import fp from "fastify-plugin";
 import { buildServiceManager } from "services/index.js";
+import type { FastifyPluginOptions } from "fastify/types/plugin.js";
 
 
 
-export default fp(async (fastify: FastifyInstance) => {
+export default fp(async (fastify: FastifyInstance, opts: FastifyPluginOptions) => {
     fastify.decorate("service", buildServiceManager(fastify));
 });
