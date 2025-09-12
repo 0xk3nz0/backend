76ccc019a0b441237fcb09f380b7eb8c37f2ebfd
enryu <67031019+0xabysss@users.noreply.github.com>
2025-09-12 13:57:41 +0100

    update: added a the auth service to the service manager.

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,6 +2,7 @@ import type { FastifyInstance } from "fastify";
 import type { ServiceManager } from "../types/service-manager.js";
 import UserService from "./user.js";
 import FriendService from "./friend.js";
+import AuthService from "./auth.js";
 
 
 
@@ -13,7 +14,8 @@ import FriendService from "./friend.js";
 export function buildServiceManager(fastify: FastifyInstance): ServiceManager {
     return {
         user: new UserService(fastify),
-        friend: new FriendService(fastify)
-        /// add other services ...
+        friend: new FriendService(fastify),
+        auth: new AuthService(fastify)
+        /// ... add other services !
     };
 }
