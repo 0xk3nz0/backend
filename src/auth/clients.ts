e921ae809246f81086a86763abf066427f6968bc
enryu <67031019+0xabysss@users.noreply.github.com>
2025-09-12 13:56:30 +0100

    update: added facebook opts ...

--- src/auth/clients.ts
+++ src/auth/clients.ts
@@ -1,5 +1,6 @@
 import type { FastifyOAuth2Options } from "@fastify/oauth2";
 import {
+    facebookOauthRedirectOpts,
     googleOauthRedirectOpts
 } from './consts.js';
 
@@ -22,4 +23,23 @@ export const googleOAuthOpts: FastifyOAuth2Options = {
     startRedirectPath: googleOauthRedirectOpts.redirectPath,
     callbackUri: googleOauthRedirectOpts.callbackUri,
     scope: [ 'profile', 'email' ]
+};
+
+export const facebookOAuthOpts: FastifyOAuth2Options = {
+    name: 'facebookOAuth2',
+    credentials: {
+        client: {
+            id: process.env.FACEBOOK_CLIENT_ID!,
+            secret: process.env.FACEBOOK_CLIENT_SECRET!
+        },
+        auth: {
+            authorizeHost: 'https://www.facebook.com',
+            authorizePath: '/v12.0/dialog/oauth',
+            tokenHost: 'https://graph.facebook.com',
+            tokenPath: '/v12.0/oauth/access_token'
+        }
+    },
+    startRedirectPath: facebookOauthRedirectOpts.redirectPath,
+    callbackUri: facebookOauthRedirectOpts.callbackUri,
+    scope: [ 'email', 'public_profile' ]
 };
